290fc1755dab8879f2920932f47afe3c116ce1db
OmarT-Y <omartamerz9@gmail.com>
2024-04-10 00:21:11 +0200

    merged 3 algos and fixed errors

--- OS_CPU_Schedulars/OS_CPU_Schedulars/abstract_sch.cpp
+++ OS_CPU_Schedulars/OS_CPU_Schedulars/abstract_sch.cpp
@@ -1,11 +1,10 @@
-#include "Definitions.h"
+#include "definitions_sch.h"
 using namespace std;
 vector<Process> Job_Q;
 int Process::number_of_tasks_created = 0;
-bool Scheduler::Check_new_processes()
+bool Scheduler::Check_new_processes(int sys_time)
 {
-	
-
+	return false;
 }
 int Scheduler::get_turnaround(Process* p,int System_time)
 {
@@ -17,13 +16,16 @@ int Scheduler::get_waiting_time(Process* p,int System_time)
 }
 bool Scheduler::check_blocked(int sys_time)
 {
+	bool added = false;
 	for (int i = 0; i < Blocked_Q.size(); i++)
 	{
 		if (Blocked_Q[i]->ariv_time >= sys_time)
 		{
-			Ready_Q.push(Blocked_Q[i]);
+			Ready_Q.push_back(Blocked_Q[i]);
+			added = true;
 		}
 	}
+	return added;
 }
 double Scheduler::get_avg_turnaround()
 {
@@ -34,6 +36,8 @@ double Scheduler::get_avg_turnaround()
 	}
 
 	avg_turnaround = avg_turnaround / turnaround.size();
+
+	return avg_turnaround;
 }
 double Scheduler::get_avg_waiting_time()
 {
@@ -44,4 +48,6 @@ double Scheduler::get_avg_waiting_time()
 	}
 
 	avg_waiting_time = avg_waiting_time / wait_time.size();
+
+	return avg_waiting_time;
 }
